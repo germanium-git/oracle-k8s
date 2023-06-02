4d98f7cd5ca9471e52a8fb6428b6ab9df3e3e654
germanium-git <31744880+germanium-git@users.noreply.github.com>
2023-06-02 10:43:11 +0200

    module virtualNetwork (#4)
    * module virtualNetwork
    
    * fixup! module virtualNetwork
    
    * fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    * fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! module virtualNetwork
    
    ---------
    
    Co-authored-by: Petr Nemec <petr.nemec@jamf.com>

---
+++ terraform/modules/virtualNetwork/variables.tf
@@ -0,0 +1,37 @@
+variable "vcn_config" {
+    type = map(object({
+        name        = string
+        vcn_cidr    = string
+    }))
+}
+
+variable "subnet_config" {
+    type = map(object({
+        name    = string
+        cidr_id = number
+        vcn_key = string
+    }))
+}
+
+
+# Protocol - Specify either all or an IPv4 protocol number as defined in Protocol Numbers
+# Options are supported only for ICMP ("1"), TCP ("6"), UDP ("17"), and ICMPv6 ("58").
+
+variable "nsg_rules" {
+  type = map(object({
+        vcn_key       = string
+        description   = string
+        source        = string
+        protocol      = string
+        tcp_options   = list(object({
+            dst_port_range_min  = number
+            dst_port_range_max  = number
+        }))
+  }))
+}
+
+
+variable "compartment_id" {
+  description = "Compartment Id"
+  type = string
+}
---
+++ terraform/modules/virtualNetwork/virtualNetwork.tf
@@ -0,0 +1,88 @@
+resource "oci_core_vcn" "vcn" {
+    for_each = var.vcn_config
+
+    display_name   = each.value.name
+    cidr_block     = each.value.vcn_cidr
+    compartment_id = var.compartment_id
+}
+
+/*
+resource "oci_core_subnet" "subnet" {
+    for_each = var.subnet_config
+
+    cidr_block        = cidrsubnet(var.vcn_config[each.value.vcn_key].vcn_cidr, 8, each.value.cidr_id)
+    display_name      = each.value.name
+    compartment_id    = var.compartment_id
+    vcn_id            = oci_core_vcn.vcn[each.value.vcn_key].id
+
+    security_list_ids = [oci_core_vcn.vcn[each.value.vcn_key].default_security_list_id]
+    route_table_id    = oci_core_route_table.rt[each.value.vcn_key].id
+}
+
+
+resource oci_core_internet_gateway "igw" {
+    for_each = var.vcn_config
+    
+    compartment_id = var.compartment_id
+    vcn_id = oci_core_vcn.vcn[each.key].id
+    display_name = "${each.value.name}-igw"
+
+    defined_tags = {}
+    enabled      = "true"
+    freeform_tags = {}
+}
+
+
+resource oci_core_route_table "rt" {
+    for_each = var.vcn_config
+
+    compartment_id = var.compartment_id
+    display_name = "Default RT for ${each.value.name}"
+    vcn_id = oci_core_vcn.vcn[each.key].id
+
+    dynamic "route_rules" {
+        for_each = each.value.routes
+        content {
+            cidr_block          = "0.0.0.0/0"
+            network_entity_id   = oci_core_internet_gateway.igw[each.key].id
+        }
+    }
+    defined_tags = {}
+    freeform_tags = {}
+}
+
+
+resource "oci_core_network_security_group" "nsg" {
+    for_each        = var.vcn_config
+    
+    display_name    =  "${each.value.name}-nsg"
+    vcn_id          = oci_core_vcn.vcn[each.key].id
+    compartment_id  = var.compartment_id
+}
+
+
+# NSG Rules
+resource "oci_core_network_security_group_security_rule" "nsg_rule" {
+    for_each = var.nsg_rules
+
+    network_security_group_id = oci_core_network_security_group.nsg[each.value.vcn_key].id
+    description               = each.value.description
+    source_type               = "CIDR_BLOCK"
+    source                    = each.value.source
+    protocol                  = each.value.protocol
+    direction                 = "INGRESS"
+    stateless                 = false
+
+    dynamic "tcp_options" {
+        for_each = {
+            for index, range in each.value.tcp_options: index => range }
+        
+        content {
+            destination_port_range {
+                max = tcp_options.dst_port_range_min
+                min = tcp_options.dst_port_range_min
+            }
+        }
+    }
+}
+*/
