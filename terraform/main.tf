8fb81221f50eb7b193bed72ee4f96e053708c583
Petr Nemec <petr.nemec@jamf.com>
2023-06-30 10:58:53 +0200

    provider block in module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,51 +3,51 @@
 #############################################
 
 module "virtual_network" {
-    source = "./modules/virtualNetwork"
+  source = "./modules/virtualNetwork"
 
-    vcn_config = {
-        vcn1 = {
-            name        = "vcn_01"
-            vcn_cidr    = "10.1.0.0/16"
-        }
+  vcn_config = {
+    vcn1 = {
+      name     = "vcn_01"
+      vcn_cidr = "10.1.0.0/16"
     }
+  }
 
-    subnet_config = {
-        subnet1 = {
-            name = "subnet1"
-            cidr_id = 1
-            vcn_key = "vcn1"
-        }
+  subnet_config = {
+    subnet1 = {
+      name    = "subnet1"
+      cidr_id = 1
+      vcn_key = "vcn1"
     }
+  }
 
-    compartment_id = oci_identity_compartment.k8s.compartment_id
+  compartment_id = oci_identity_compartment.k8s.compartment_id
 
-    nsg_rules = {
-        rule1 = {
-            vcn_key       = "vcn1"
-            description   = "ssh"
-            source        = "185.230.172.74/32"
-            protocol      = "6"
-            dst_port_range_min  = 22
-            dst_port_range_max  = 22
-        }
-        rule2 = {
-            vcn_key       = "vcn1"
-            description   = "influxdb"
-            source        = "185.230.172.74/32"
-            protocol      = "6"
-            dst_port_range_min  = 8086
-            dst_port_range_max  = 8086
-        }
-        rule3 = {
-            vcn_key       = "vcn1"
-            description   = "portainer"
-            source        = "185.230.172.74/32"
-            protocol      = "6"
-            dst_port_range_min  = 9000
-            dst_port_range_max  = 9000
-        }
+  nsg_rules = {
+    rule1 = {
+      vcn_key            = "vcn1"
+      description        = "ssh"
+      source             = "185.230.172.74/32"
+      protocol           = "6"
+      dst_port_range_min = 22
+      dst_port_range_max = 22
     }
+    rule2 = {
+      vcn_key            = "vcn1"
+      description        = "influxdb"
+      source             = "185.230.172.74/32"
+      protocol           = "6"
+      dst_port_range_min = 8086
+      dst_port_range_max = 8086
+    }
+    rule3 = {
+      vcn_key            = "vcn1"
+      description        = "portainer"
+      source             = "185.230.172.74/32"
+      protocol           = "6"
+      dst_port_range_min = 9000
+      dst_port_range_max = 9000
+    }
+  }
 }
 
 #############################################
@@ -60,11 +60,11 @@ module "virtual_network" {
 # Oracle-Linux-8.6-aarch64-2022.05.30-0 ocid1.image.oc1.eu-frankfurt-1.aaaaaaaaxp2zqotv37r4zycmwfqywcujsh4scenphjjs5w2ozakmidg3vs6q
 
 module "virtual_machine" {
-    source = "./modules/virtualMachine"
+  source = "./modules/virtualMachine"
 
-    compartment_id = oci_identity_compartment.k8s.compartment_id
-    vm_config = {
-        /*
+  compartment_id = oci_identity_compartment.k8s.compartment_id
+  vm_config = {
+    /*
         control1 = {
             shape       = "VM.Standard.A1.Flex"
             cpu_count   = 2
@@ -86,25 +86,25 @@ module "virtual_machine" {
             zone        = 2
         }
         */
-        worker1 = {
-            shape       = "VM.Standard.E2.1.Micro"
-            cpu_count   = 1
-            memory_gb   = 1
-            image_id    = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaazjh7dx5267q4cpzeg7fgxhyluqq42usze6ahijkrs6bnwg2d2mdq"
-            subnet_id   = module.virtual_network.subnet["subnet1"].id
-            nsg_ids     = toset([module.virtual_network.nsg["vcn1"].id])
-            ssh_key     = file("sshkey/petr.nemec@gmx.com_2023-05-21T20_18_59.854Z_putty.pub")
-            zone        = 2
-        }
-        worker2 = {
-            shape       = "VM.Standard.E2.1.Micro"
-            cpu_count   = 1
-            memory_gb   = 1
-            image_id    = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaazjh7dx5267q4cpzeg7fgxhyluqq42usze6ahijkrs6bnwg2d2mdq"
-            subnet_id   = module.virtual_network.subnet["subnet1"].id
-            nsg_ids     = toset([module.virtual_network.nsg["vcn1"].id])
-            ssh_key     = file("sshkey/petr.nemec@gmx.com_2023-05-21T20_18_59.854Z_putty.pub")
-            zone        = 2
-        }
+    worker1 = {
+      shape     = "VM.Standard.E2.1.Micro"
+      cpu_count = 1
+      memory_gb = 1
+      image_id  = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaazjh7dx5267q4cpzeg7fgxhyluqq42usze6ahijkrs6bnwg2d2mdq"
+      subnet_id = module.virtual_network.subnet["subnet1"].id
+      nsg_ids   = toset([module.virtual_network.nsg["vcn1"].id])
+      ssh_key   = file("sshkey/petr.nemec@gmx.com_2023-05-21T20_18_59.854Z_putty.pub")
+      zone      = 2
     }
+    worker2 = {
+      shape     = "VM.Standard.E2.1.Micro"
+      cpu_count = 1
+      memory_gb = 1
+      image_id  = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaazjh7dx5267q4cpzeg7fgxhyluqq42usze6ahijkrs6bnwg2d2mdq"
+      subnet_id = module.virtual_network.subnet["subnet1"].id
+      nsg_ids   = toset([module.virtual_network.nsg["vcn1"].id])
+      ssh_key   = file("sshkey/petr.nemec@gmx.com_2023-05-21T20_18_59.854Z_putty.pub")
+      zone      = 2
+    }
+  }
 }
--- terraform/modules/virtualNetwork/virtualNetwork.tf
+++ terraform/modules/virtualNetwork/virtualNetwork.tf
@@ -1,79 +1,79 @@
 resource "oci_core_vcn" "vcn" {
-    for_each = var.vcn_config
+  for_each = var.vcn_config
 
-    display_name   = each.value.name
-    cidr_block     = each.value.vcn_cidr
-    compartment_id = var.compartment_id
+  display_name   = each.value.name
+  cidr_block     = each.value.vcn_cidr
+  compartment_id = var.compartment_id
 }
 
 
 resource "oci_core_subnet" "subnet" {
-    for_each = var.subnet_config
+  for_each = var.subnet_config
 
-    cidr_block        = cidrsubnet(var.vcn_config[each.value.vcn_key].vcn_cidr, 8, each.value.cidr_id)
-    display_name      = each.value.name
-    compartment_id    = var.compartment_id
-    vcn_id            = oci_core_vcn.vcn[each.value.vcn_key].id
+  cidr_block     = cidrsubnet(var.vcn_config[each.value.vcn_key].vcn_cidr, 8, each.value.cidr_id)
+  display_name   = each.value.name
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn[each.value.vcn_key].id
 
-    security_list_ids = [oci_core_vcn.vcn[each.value.vcn_key].default_security_list_id]
-    route_table_id    = oci_core_route_table.rt[each.value.vcn_key].id
+  security_list_ids = [oci_core_vcn.vcn[each.value.vcn_key].default_security_list_id]
+  route_table_id    = oci_core_route_table.rt[each.value.vcn_key].id
 }
 
 
-resource oci_core_internet_gateway "igw" {
-    for_each = var.vcn_config
-    
-    compartment_id = var.compartment_id
-    vcn_id = oci_core_vcn.vcn[each.key].id
-    display_name = "${each.value.name}-igw"
+resource "oci_core_internet_gateway" "igw" {
+  for_each = var.vcn_config
 
-    defined_tags = {}
-    enabled      = "true"
-    freeform_tags = {}
+  compartment_id = var.compartment_id
+  vcn_id         = oci_core_vcn.vcn[each.key].id
+  display_name   = "${each.value.name}-igw"
+
+  defined_tags  = {}
+  enabled       = "true"
+  freeform_tags = {}
 }
 
 
-resource oci_core_route_table "rt" {
-    for_each = var.vcn_config
+resource "oci_core_route_table" "rt" {
+  for_each = var.vcn_config
 
-    compartment_id = var.compartment_id
-    display_name = "Default RT for ${each.value.name}"
-    vcn_id = oci_core_vcn.vcn[each.key].id
+  compartment_id = var.compartment_id
+  display_name   = "Default RT for ${each.value.name}"
+  vcn_id         = oci_core_vcn.vcn[each.key].id
 
-    route_rules {
-        destination         = "0.0.0.0/0"
-        network_entity_id   = oci_core_internet_gateway.igw[each.key].id
-        destination_type    = "CIDR_BLOCK"
-    }
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_internet_gateway.igw[each.key].id
+    destination_type  = "CIDR_BLOCK"
+  }
 
-    defined_tags = {}
-    freeform_tags = {}
+  defined_tags  = {}
+  freeform_tags = {}
 }
 
 
 resource "oci_core_network_security_group" "nsg" {
-    for_each        = var.vcn_config
-    
-    display_name    =  "${each.value.name}-nsg"
-    vcn_id          = oci_core_vcn.vcn[each.key].id
-    compartment_id  = var.compartment_id
+  for_each = var.vcn_config
+
+  display_name   = "${each.value.name}-nsg"
+  vcn_id         = oci_core_vcn.vcn[each.key].id
+  compartment_id = var.compartment_id
 }
 
 # Allows only TCP protocol
 resource "oci_core_network_security_group_security_rule" "nsg_rule" {
-    for_each = var.nsg_rules
+  for_each = var.nsg_rules
 
-    network_security_group_id = oci_core_network_security_group.nsg[each.value.vcn_key].id
-    description               = each.value.description
-    source_type               = "CIDR_BLOCK"
-    source                    = each.value.source
-    protocol                  = "6"
-    direction                 = "INGRESS"
-    stateless                 = false
-    tcp_options {
-        destination_port_range {
-            max = each.value.dst_port_range_max
-            min = each.value.dst_port_range_min
-        }
+  network_security_group_id = oci_core_network_security_group.nsg[each.value.vcn_key].id
+  description               = each.value.description
+  source_type               = "CIDR_BLOCK"
+  source                    = each.value.source
+  protocol                  = "6"
+  direction                 = "INGRESS"
+  stateless                 = false
+  tcp_options {
+    destination_port_range {
+      max = each.value.dst_port_range_max
+      min = each.value.dst_port_range_min
     }
+  }
 }
