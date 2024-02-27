0debe6e0f43c421743230672614dc6937969858d
germanium-git <germanium-git@gmx.com>
2024-02-27 16:23:17 +0100

    port 80 acme.sh

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,8 +54,6 @@ module "virtual_network" {
       protocol           = "6"
       dst_port_range_min = 80
       dst_port_range_max = 80
-      dst_port_range_min = 9443
-      dst_port_range_max = 9443
     }
   }
 }
